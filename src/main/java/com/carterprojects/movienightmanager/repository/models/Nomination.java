b58daa927564e3f062ce858c631aa2ffea12d771
CarterW715 <carterw8989@hotmail.com>
2023-11-01 18:30:43 -0500

    add in api functionality to be able to delete a nomination

--- src/main/java/com/carterprojects/movienightmanager/repository/models/Nomination.java
+++ src/main/java/com/carterprojects/movienightmanager/repository/models/Nomination.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import jakarta.persistence.*;
+
 import java.util.List;
 
 @Data
@@ -35,7 +36,7 @@ public class Nomination {
     @JoinColumn(name = "movie_night_segment_id", nullable = false)
     MovieNightSegment movieNightSegment;
 
-    @OneToMany(mappedBy = "nomination")
+    @OneToMany(mappedBy = "nomination", cascade = CascadeType.REMOVE, orphanRemoval = true)
     List<NominationLike> nominationLikes;
 
     @ManyToOne(optional = false)
