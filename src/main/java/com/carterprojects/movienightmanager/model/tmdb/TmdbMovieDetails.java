532c2dfa258b0da122dce71a17325bf879ea0d3f
hzyniecki <hzyniecki@gmail.com>
2024-02-27 12:21:12 -0600

    Appended credits querty param to the end of details request

--- src/main/java/com/carterprojects/movienightmanager/model/tmdb/TmdbMovieDetails.java
+++ src/main/java/com/carterprojects/movienightmanager/model/tmdb/TmdbMovieDetails.java
@@ -13,12 +13,28 @@ public class TmdbMovieDetails {
     Integer runtime;
     List<TmdbMovieGenre> genres;
     String overview;
+    TmdbMovieCredits credits;
 
     @Data
     @AllArgsConstructor
     @NoArgsConstructor
     public static class TmdbMovieGenre {
         Integer id;
+        String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class TmdbMovieCredits {
+        List<TmdbMovieCast> cast;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class TmdbMovieCast {
+        String known_for_department;
         String name;
     }
 }
